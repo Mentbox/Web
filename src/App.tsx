5d5cc450600bb93c1f864b4e60630ee6f5204894
e-tae-hyeon <johtta888@naver.com>
2025-05-05 09:48:48 +0900

    feat: 추가 react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "./app/_root";
+import queryClient from "./common/query-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

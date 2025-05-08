373ac0c240b08b3f5df40ec2acb3ae0b08a4d7fd
e-tae-hyeon <johtta888@naver.com>
2025-05-08 14:35:46 +0900

    feat: 추가 history plugin to stack flow

--- src/app/_root.ts
+++ src/app/_root.ts
@@ -3,17 +3,27 @@ import { stackflow } from "@stackflow/react";
 import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
 import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
 import HomeScreen from "./home";
+import { historySyncPlugin } from "@stackflow/plugin-history-sync";
+import TestScreen from "./test";
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 350,
   activities: {
     HomeScreen,
+    TestScreen,
   },
   initialActivity: () => "HomeScreen",
   plugins: [
     basicRendererPlugin(),
     basicUIPlugin({
       theme: "cupertino",
+    }),
+    historySyncPlugin({
+      routes: {
+        HomeScreen: "/",
+        TestScreen: "/test",
+      },
+      fallbackActivity: () => "HomeScreen",
     }),
   ],
 });

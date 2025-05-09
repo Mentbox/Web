0ab116a071ca50e1dc629b341dab24edfa6ce37d
e-tae-hyeon <johtta888@naver.com>
2025-05-09 12:11:49 +0900

    feat: 추가 write file date selector
    react-calendar 이용
    월간 달력만 적용 위해 patch package로 해당 라이브러리 수정

--- src/app/files/write.tsx
+++ src/app/files/write.tsx
@@ -2,6 +2,7 @@ import { AppScreen } from "@stackflow/plugin-basic-ui";
 import { ActivityComponentType } from "@stackflow/react";
 import Header from "../../components/Header";
 import WriteFileNameForm from "../../features/files/modules/WriteFileNameForm";
+import WriteFileDateSelector from "../../features/files/modules/WriteFileDateSelector";
 
 const WriteFileScreen: ActivityComponentType = () => {
   return (
@@ -9,8 +10,10 @@ const WriteFileScreen: ActivityComponentType = () => {
       <div className="screen">
         <Header.Arrow title="파일 작성" />
 
-        <main className="flex-1 flex flex-col p-[16px]">
+        <main className="flex-1 flex flex-col gap-[24px] p-[16px]">
           <WriteFileNameForm />
+
+          <WriteFileDateSelector />
         </main>
 
         <section className="px-[16px] py-[16px] pb-[40px] flex flex-col">
--- src/components/Icon/icons.ts
+++ src/components/Icon/icons.ts
@@ -9,3 +9,4 @@ export { default as 직장인 } from "@icons/직장인.svg?react";
 export { default as 취준 } from "@icons/취준.svg?react";
 export { default as 팀플 } from "@icons/팀플.svg?react";
 export { default as 프로젝트 } from "@icons/프로젝트.svg?react";
+export { default as chevronRight } from "@icons/chevron-right.svg?react";

ffdda681dad8490ab4f2881044fcc77dc47ddd48
e-tae-hyeon <johtta888@naver.com>
2025-05-09 12:35:49 +0900

    publish: write file screen

--- src/components/Icon/icons.ts
+++ src/components/Icon/icons.ts
@@ -1,2 +1,6 @@
 export { default as chevronLeft } from "@icons/chevron-left.svg?react";
 export { default as chevronRight } from "@icons/chevron-right.svg?react";
+export { default as chevronUp } from "@icons/chevron-up.svg?react";
+export { default as chevronDown } from "@icons/chevron-down.svg?react";
+export { default as copy } from "@icons/copy.svg?react";
+export { default as remove } from "@icons/remove.svg?react";

8390dd242571d368f72f03155b16005e9f2e9c86
mkjun2016 <mkjun2016@naver.com>
2025-05-10 10:56:07 +0900

    add: 추가 Login Btn 컴포넌트 추가

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -4,6 +4,7 @@ import getTheme from "../common/styles/theme";
 import kakao from "@icons/kakao.svg"
 import apple from "@icons/apple.svg"
 import Logo from "../components/Logo";
+import LoginButton from "../components/LoginButton";
 
 const LoginScreen: ActivityComponentType = () => {
   const theme = getTheme();
@@ -16,15 +17,8 @@ const LoginScreen: ActivityComponentType = () => {
           <h1 className="h1">오늘의 연습을 시작해볼까요?</h1>
         </div>
         <main className="flex flex-col gap-[24px]">
-          {/** @todo 로그인 btn 구현 후 교체 */}
-          <button style={{ backgroundColor: "#FEE500" }} className="flex flex-row items-center justify-center h-[45px] text-[1.125rem] leading-[1.05] rounded-md font-medium transition duration-150 ease-in-out text-black p-[16px]">
-            <img src={kakao} width={18} className="absolute left-[40px]" />
-            <span className="text-[15px]">카카오 로그인</span>
-          </button>
-          <button style={{ backgroundColor: "#000000" }} className="flex flex-row items-center justify-center h-[45px] text-[1.125rem] leading-[1.05] rounded-md font-medium transition duration-150 ease-in-out text-white p-[16px]">
-            <img src={apple} width={18} className="absolute left-[40px]" />
-            <span className="text-[15px]">Apple로 로그인</span>
-          </button>
+          <LoginButton src={kakao} color="#FEE500">카카오 로그인</LoginButton>
+          <LoginButton src={apple} color="#000000">Apple로 로그인</LoginButton>
         </main>
       </div>
     </AppScreen>
